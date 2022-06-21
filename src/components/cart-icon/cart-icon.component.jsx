32bcd62b959218d19c6335ef5fbb55f413070458
Steven Li <sxli@buffalo.edu>
2022-06-21 15:43:00 -0400

    Shop Dropdown functionality works now

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,12 @@
 import './cart-icon.styles.scss';
 import {ReactComponent as ShopLogo} from '../../assets/shopping-bag.svg';
-
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 const CartIcon = () => {
+    const { isCartOpen, setIsCartOpen} = useContext(CartContext);
+    const toggleCartOpen = () => setIsCartOpen(!isCartOpen);
     return(
-        <div className = 'cart-icon-container'>
+        <div className = 'cart-icon-container' onClick = {toggleCartOpen}>
             <ShopLogo className = 'shopping-icon'/>
             <span className = 'item-count'>0</span>
         </div>
