f6c62fdd9d02168fadbc77e5213f0a274e5d37e3
Steven Li <sxli@buffalo.edu>
2022-06-22 00:04:18 -0400

    Fixed main page and shop child components css

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,13 @@
-import { useContext, Fragment } from "react";
 import './shop.styles.scss';
-import { CategoriesContext } from "../../context/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route} from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 const Shop = () => {
-    const {categories} = useContext(CategoriesContext);
     return(
-        <Fragment>
-            {
-                Object.keys(categories).map( title => (
-                    <Fragment key = {title}>
-                    <h2>{title}</h2>
-                    
-                    <div className = 'products-container'>
-                    { categories[title].map( (product) => (
-                        <ProductCard key = {product.id} product = {product}/>
-                    )) };
-                    </div>
-                    </Fragment>
-                ))
-            }
-            
-        </Fragment>
+        <Routes>
+            <Route index element = {<CategoriesPreview/>} />
+            <Route path = ":category" element = {<Category/>} />
+        </Routes>
     );
 };
 
